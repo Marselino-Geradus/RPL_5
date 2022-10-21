9629b616ee0755742972a2936253a8c3607abf03
Marselino-Geradus <marselino914@gmail.com>
2022-10-21 18:15:19 +0800

    Day 5

---
+++ Tipe data List.py
@@ -0,0 +1,22 @@
+#Tipe  data List
+#1. List kosong
+a= []
+#1. List berisi String
+buah = ['mangga','pisang','nangka',"apel"]
+#1. List berisi integer
+nilai_siswa = [80,80,30,40,67,75]
+#1. List berisi banyak tipe data
+campuran = [88,"Marsel","Rizal",2.0,True,False,90,100]
+
+
+print(a)
+print(buah)
+print(nilai_siswa)
+print(campuran)
+
+#cara mengakses nilai list
+print(buah[3])
+print(campuran[:3])
+print(campuran[3:])
+print(campuran[1:][:4])
+
